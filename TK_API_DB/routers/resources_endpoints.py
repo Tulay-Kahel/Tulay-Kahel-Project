3fd076d01bcab01aeefb8d9604a760f86d4286a9
John Markton Olarte <jmolarte@up.edu.ph>
2022-12-09 22:47:27 +0800

    Initialized Additional Endpoints for Resources

--- TK_API_DB/routers/resources_endpoints.py
+++ TK_API_DB/routers/resources_endpoints.py
@@ -7,6 +7,13 @@ router = APIRouter(
 
 ####### RESOURCES ######################################################################################
 
+#### DIRECTORIES
+
+# Add a directory
+@router.post("/directories", tags=["Resources"])
+async def add_directory(directory: str):
+    return {"message": "Add a directory"}
+
 # Get all directories
 # This are the directories for VAWC related contacts and resources
 @router.get("/directories", tags=["Resources"])
@@ -19,5 +26,28 @@ async def get_directory(directory_name: str):
     directory_name = directory_name.lower()
     return {"message": "Get a specific directory"}
 
+####
+
+
+#### INFORMATION
+
+# Add an information
+@router.post("/informations", tags=["Resources"])
+async def add_information(information: str):
+    return {"message": "Add an information"}
+
+# Get all informations
+# These are the informations about VAWC
+@router.get("/informations", tags=["Resources"])
+async def get_all_informations():
+    return {"message": "Get all informations"}
+
+# Get a specific information based on the title of the information
+@router.get("/informations/{info_title}", tags=["Resources"])
+async def get_information(info_title: str):
+    info_title = info_title.lower()
+    return {"message": "Get a specific information"}
+
+####
 
 ########################################################################################################
