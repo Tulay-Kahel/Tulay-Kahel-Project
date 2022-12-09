16f682d55b1738aebd18dcb86a2c63d31381620d
John Markton Olarte <jmolarte@up.edu.ph>
2022-12-09 19:00:56 +0800

    Added Temp AppBar to Admin Console Page

--- TK_webApp/tk_webapp/lib/pages/admin_console.dart
+++ TK_webApp/tk_webapp/lib/pages/admin_console.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:tk_webapp/static_elements/app_bar.dart';
 
 class AdminConsole extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Admin Console"),
-      ),
+      // Temporary AppBar (Adnmin Console will have a different AppBar)
+      appBar: app_bar(context, "admin_console"),
       body: const Center(
         child: Text("Admin Console"),
       ),
