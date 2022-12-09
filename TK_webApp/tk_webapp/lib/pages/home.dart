1b8497c425eaf6d8b467c6525aa387dae7514f15
John Markton Olarte <jmolarte@up.edu.ph>
2022-12-09 19:01:14 +0800

    Added AppBar to Home Page

--- TK_webApp/tk_webapp/lib/pages/home.dart
+++ TK_webApp/tk_webapp/lib/pages/home.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tk_webapp/static_elements/app_bar.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home"),
-      ),
+      appBar: app_bar(context, "home"),
       body: const Center(
         child: Text("Home"),
       ),
